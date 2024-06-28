d361ed4813c699858c25a8fd126a2ec825be0a2a
Rafaela Chagas <144394731+Harunookami@users.noreply.github.com>
2024-06-28 20:07:42 -0300

    11 de 100

---
+++ Dia 11/src/SomaAlgoritmosJava.java
@@ -0,0 +1,13 @@
+public class SomaAlgoritmosJava {
+    public static void main(String[] args){
+        
+        int soma = 0;
+
+        for (int i = 1; i <= 1500; i++){
+            soma +=i;
+        }
+
+        System.out.println("A soma dos números de 1 a 15 é: " + soma);
+        
+    }
+}
